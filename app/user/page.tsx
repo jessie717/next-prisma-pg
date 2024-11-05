d1d64023dbdd94bab6476b35f49550cbdff18e8b
jessie717 <jessiezly717@gmail.com>
2024-11-05 14:02:49 +0800

    prisma 连接supabase示例

---
+++ app/user/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className='h-full flex justify-center items-center'>
+        <div className='text-2xl text-slate-200'>user test</div>
+      </div>
+    )
+  }
+  
